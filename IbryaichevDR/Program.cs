04b0cf239ab9160344d69492db4047ef1ddb9b7a
NorthCountr13 <dmitriyibryaichev@mail.ru>
2026-04-05 17:04:30 +0300

    Add project files.

---
+++ IbryaichevDR/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IbryaichevDR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

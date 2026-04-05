bca84de7f65ad9bf28ded6b1cd9cf8a9447014fc
NorthCountr13 <dmitriyibryaichev@mail.ru>
2026-04-05 17:14:37 +0300

    add new feature

--- IbryaichevDR/Program.cs
+++ IbryaichevDR/Program.cs
@@ -10,3 +10,23 @@
     // Ибряйчев Дмитрий Романович Ам-1-25
 }
 //solve task2
+
+
+namespace YourProjectName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // тест функции (можно потом удалить или оставить)
+            double result = Sqr(5);
+            Console.WriteLine(result); // 25
+        }
+
+        // Добавляемая функция
+        static public double Sqr(double X)
+        {
+            return X * X;
+        }
+    }
+}

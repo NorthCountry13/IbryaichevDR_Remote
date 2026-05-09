36f7eae989d006e558ed1f5ac8ba4e9eecdbf062
NorthCountr13 <dmitriyibryaichev@mail.ru>
2026-05-09 15:43:38 +0300

    2

--- IbryaichevDR/Program.cs
+++ IbryaichevDR/Program.cs
@@ -4,31 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
         }
+        //Изменение
+        //IbryaichevDR, Am-1-25
+        //ЛР 2.2
+        ////Updarw for task 7
+        ////new_feature
+        ///solve task1
+        ///solve task2
     }
-    // Ибряйчев Дмитрий Романович Ам-1-25
-}
 
-
-
-namespace YourProjectName
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // тест функции (можно потом удалить или оставить)
-            double result = Sqr(5);
-            Console.WriteLine(result); // 25
-        }
-
-        // Добавляемая функция
-        static public double Sqr(double X)
-        {
-            return X * X;
-        }
-        //solve task1
-
-    }
 }
